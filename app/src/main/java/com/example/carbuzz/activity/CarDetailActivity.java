25a2e0e2bbe2d5f4b728108db1c7b017a779feef
Vishal <vishal.salyan@gmail.com>
2020-04-04 22:23:52 +0530

    CarDetail and profile Activity

---
+++ app/src/main/java/com/example/carbuzz/activity/CarDetailActivity.java
@@ -0,0 +1,17 @@
+package com.example.carbuzz.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.carbuzz.R;
+
+public class CarDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_detail);
+
+    }
+}
---
+++ app/src/main/java/com/example/carbuzz/activity/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.carbuzz.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.carbuzz.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}

3c8a16cfe6d147c873466c3af8ead609663c0862
dharamsingh <jsracer71@gmail.com>
2020-04-06 01:42:33 -0400

    search

--- app/src/main/java/com/example/carbuzz/fragment/SearchFragment.java
+++ app/src/main/java/com/example/carbuzz/fragment/SearchFragment.java
@@ -1,19 +1,28 @@
 package com.example.carbuzz.fragment;
 
+import android.app.SearchManager;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import com.example.carbuzz.R;
 
 
 public class SearchFragment extends Fragment {
+
+    private SearchView searchView = null;
+    private SearchView.OnQueryTextListener queryTextListener;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -24,13 +33,21 @@ public class SearchFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
     }
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
+
+
 
 }
